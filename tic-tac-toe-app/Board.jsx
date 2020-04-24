1be3bbb2ae871628e9cffefa7457bed74e38a4cc
Carmen Salas <carmensalas@Carmens-MacBook-Pro.local>
2020-04-24 02:00:08 -0400

    logic almost done

--- tic-tac-toe-app/Board.jsx
+++ tic-tac-toe-app/Board.jsx
@@ -8,7 +8,7 @@ function Board() {
   // state for the next turn
   const [isXPlayer, setXPlayer] = React.useState(true);
 
-// fucntion for when a sqaure is clicked
+  // fucntion for when a sqaure is clicked
   const whenClicked = index => {
     // spread the items in boardSquare to new array to make a copy
     const squares = [...boardSquares];
@@ -31,12 +31,16 @@ function Board() {
     );
   };
 
-  // create the gameStatus to determine next player
-  const gameStatus = isXPlayer ? 'X' : 'O';
+  // create the gameStatus to determine next player and winner
+  let gameStatus;
+  const winner = findWinner(boardSquares);
+  gameStatus = winner
+    ? `Winner is ${winner}!`
+    : `It is${isXPlayer ? 'X' : 'O'}'s Turn...'`;
 
   return (
     <div className="board">
-      <h2 className={gameStatus}>It is {gameStatus}'s turn</h2>
+      <h2 className={gameStatus}>{gameStatus}</h2>
       <div className="boardRow">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -48,10 +52,36 @@ function Board() {
         {renderSquare(5)}
       </div>
       <div className="boardRow">
+        {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-        {renderSquare(9)}
       </div>
     </div>
   );
+}
+
+// function to determine winner
+const findWinner = (squares) => {
+  // winning combinations
+
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < winningCombos.length; i++) {
+    // destructure winningCombos[i] array
+    const [a, b, c] = winningCombos[i];
+    // returning x or o if squares match
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[b]) {
+      return squares[a];
+    }
+  }
+  // else return null
+  return null;
 }
